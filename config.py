5ca116089ac89fb16523cd747dc0fd4b681f79ed
arclx724 <arclx724@gmail.com>
2026-01-21 16:38:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,10 @@
-# ============================================================
-#Group Manager Bot
-# Author: LearningBotsOfficial (https://github.com/LearningBotsOfficial) 
-# Support: https://t.me/LearningBotsCommunity
-# Channel: https://t.me/learning_bots
-# YouTube: https://youtube.com/@learning_bots
-# License: Open-source (keep credits, no resale)
-# ============================================================
-
 import os
+from dotenv import load_dotenv  # <--- Yeh line zaroori hai
 
-# Required configurations (loaded from environment variables)
+# Load .env file
+load_dotenv()
+
+# Required configurations
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
