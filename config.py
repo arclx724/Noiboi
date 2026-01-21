480daf72b944be51f6ed87970fbd848b4d7307c1
arclx724 <arclx724@gmail.com>
2026-01-21 16:24:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_URI = os.getenv("MONGO_URI", "")
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
 
 # Owner and bot details
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
