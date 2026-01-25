52f5c7d0406686392f2e9071a332fcb7642aa728
arclx724 <arclx724@gmail.com>
2026-01-25 22:52:10 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,7 +3,8 @@ from .group_commands import register_group_commands
 from .anti_abuse import register_abuse_handlers
 from .anti_nuke import register_anti_nuke
 from .clean_service import register_clean_service_handlers
-from .media_delete import register_media_delete_handlers  # <--- NEW IMPORT
+from .media_delete import register_media_delete_handlers
+from .anti_bots import register_antibot_handlers  # <--- NEW IMPORT
 
 def register_all_handlers(app):
     register_handlers(app)
@@ -11,9 +12,10 @@ def register_all_handlers(app):
     register_abuse_handlers(app)
     register_anti_nuke(app)
     register_clean_service_handlers(app)
+    register_media_delete_handlers(app)
     
-    # Register New Plugin
-    register_media_delete_handlers(app)  # <--- NEW CALL
+    # Register Anti-Bot
+    register_antibot_handlers(app)  # <--- NEW CALL
     
-    print("✅ All Handlers Loaded: Start, Group, Abuse, Anti-Nuke, Clean-Service, Media-Delete")
+    print("✅ All Handlers Loaded: Start, Group, Abuse, Nuke, Clean, Media, Anti-Bot")
     
